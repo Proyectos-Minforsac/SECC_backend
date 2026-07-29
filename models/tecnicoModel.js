15a4ce49c2a9a20be9170224ddf1ddeb5f3a2d77
AcherPlus <enmanuel.obando@unmsm.edu.pe>
2026-07-28 22:22:12 -0500

    nueva consulta para tecnicos y precios_aire

--- models/tecnicoModel.js
+++ models/tecnicoModel.js
@@ -1,17 +1,35 @@
 const db = require('../config/database');
 
 async function getAllTecnicos(page, limit, search) {
-  console.log('Modelo: solicitando todos los técnicos');
+  // console.log('Modelo: solicitando todos los técnicos');
 
   const offset = (page - 1) * limit;
 
-  const tecnicos = await db.query(`
-    SELECT *
-    FROM "tecnicos"
-    WHERE nombre ILIKE '%' || $3 || '%'
-    ORDER BY tecnico_id DESC
+  const tecnicos = await db.query(
+    `
+    SELECT
+      t.*,
+      COALESCE(
+        json_agg(
+          json_build_object(
+            'precio_aire_id', pa.precio_aire_id,
+            'tipo_aire', pa.tipo_aire,
+            'precio', pa.precio
+          )
+          ORDER BY pa.tipo_aire
+        ) FILTER (WHERE pa.precio_aire_id IS NOT NULL),
+        '[]'
+      ) AS precios
+    FROM "tecnicos" t
+    LEFT JOIN "precios_aire" pa
+      ON t.tecnico_id = pa.tecnico_id
+    WHERE t.nombre ILIKE '%' || $3 || '%'
+    GROUP BY t.tecnico_id
+    ORDER BY t.tecnico_id ASC
     LIMIT $1 OFFSET $2
   `, [limit, offset, search]);
+
+  console.log(JSON.stringify(tecnicos.rows, null, 2))
 
   const total = await db.query(`
     SELECT COUNT(*) AS total
@@ -19,8 +37,13 @@ async function getAllTecnicos(page, limit, search) {
     WHERE nombre ILIKE '%' || $1 || '%'
   `, [search]);
 
+  const tecnicosConPrecios = tecnicos.rows.map((tecnico) => ({
+    ...tecnico,
+    precios: typeof tecnico.precios === 'string' ? JSON.parse(tecnico.precios) : tecnico.precios,
+  }));
+
   return {
-    tecnicos: tecnicos.rows,
+    tecnicos: tecnicosConPrecios,
     total: Number(total.rows[0].total),
     page,
     limit,
@@ -28,9 +51,10 @@ async function getAllTecnicos(page, limit, search) {
   };
 }
 
-async function createTecnico({nombre, tipo_documento, numero_documento, telefono, ubicacion, servicio, area, calificacion}) {
+async function createTecnico({ nombre, tipo_documento, numero_documento, telefono, ubicacion, servicio, area, calificacion }) {
   console.log('Modelo: creando técnico: ', {
-  nombre, tipo_documento, numero_documento, telefono, ubicacion, servicio, area, calificacion });
+    nombre, tipo_documento, numero_documento, telefono, ubicacion, servicio, area, calificacion
+  });
 
   const result = await db.query(
 
@@ -38,7 +62,7 @@ async function createTecnico({nombre, tipo_documento, numero_documento, telefono
 }
 
 async function updateTecnico(tecnico_id, {
-nombre, tipo_documento, numero_documento, telefono, ubicacion, servicio, area, calificacion }) {
+  nombre, tipo_documento, numero_documento, telefono, ubicacion, servicio, area, calificacion }) {
   console.log('Modelo: actualizando técnico', tecnico_id);
 
   const result = await db.query(
